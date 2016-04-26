f78c89c15f56dbc77a38049b112a298168d091d9
Rajasingh <rajasingh@egbsystems.com>
2016-04-26 13:12:41 +0530

    Volley Example project created.

---
+++ app/src/main/java/AppController.java
@@ -0,0 +1,49 @@
+import android.app.Application;
+import android.text.TextUtils;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+
+/**
+ * Created by rajasingh on 4/26/2016.
+ */
+public class AppController extends Application {
+
+    private static AppController mInstance;
+    private RequestQueue mRequestQueue;
+    public static final String TAG = AppController.class.getSimpleName();
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+    public static synchronized AppController getInstance(){
+        return mInstance;
+    }
+
+    public RequestQueue getRequestQueue(){
+        if(mRequestQueue == null){
+            mRequestQueue = Volley.newRequestQueue(getApplicationContext());
+        }
+        return mRequestQueue;
+    }
+
+    public <T> void addToRequestQueue(Request<T> request, String tag){
+        request.setTag(TextUtils.isEmpty(tag) ? TAG : tag);
+        getRequestQueue().add(request);
+    }
+
+    public <T> void addToRequestQueue(Request<T> request){
+        request.setTag(TAG);
+        getRequestQueue().add(request);
+    }
+
+    public void cancelPendingRequest(Object tag){
+        if(mRequestQueue != null){
+            mRequestQueue.cancelAll(tag);
+        }
+    }
+
+}
