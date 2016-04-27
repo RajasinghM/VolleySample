cdfd50ae70c3eebbfce9fcd07455c287c6a4d62a
Rajasingh <rajasingh@egbsystems.com>
2016-04-27 12:51:56 +0530

    Error cleared, Getting the data from the server.

--- app/src/main/java/com/volleyexample/MainActivity.java
+++ app/src/main/java/com/volleyexample/MainActivity.java
@@ -13,10 +13,14 @@ import android.widget.EditText;
 
 import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
+import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.lang.reflect.Method;
@@ -26,7 +30,8 @@ import java.util.Map;
 public class MainActivity extends Activity implements View.OnClickListener {
 
     private String TAG = MainActivity.class.getSimpleName();
-    String url = "http://192.168.1.18/245webserver/trackertest/webservice/checkvendor/";
+//    String url = "http://192.168.1.18/245webserver/trackertest/webservice/checkvendor/";
+    String url = "http://api.geonames.org/citiesJSON?north=44.1&south=-9.9&east=-22.4&west=55.2&lang=de&username=demo";
     String strUsername, strPassword, strGroupname;
     EditText uName, pWord, gpName;
     ProgressDialog mProgress;
@@ -48,13 +53,27 @@ public class MainActivity extends Activity implements View.OnClickListener {
         register.setOnClickListener(this);
     }
 
-        private void getJsonObjectReq(final String strUsername, final String strPassword, final String strGroupname){
+        private void getJsonObjectReq(){
+            strUsername = uName.getText().toString().trim();
+            strPassword = pWord.getText().toString().trim();
+            strGroupname = gpName.getText().toString().trim();
             showProgressDialog();
             JsonObjectRequest jObjReq = new JsonObjectRequest(Request.Method.POST, url, null,
                     new Response.Listener<JSONObject>() {
                 @Override
                 public void onResponse(JSONObject jsonObject) {
-                    Log.d(TAG + "Response", jsonObject.toString());
+                    Log.d(TAG + "---> Response <---", jsonObject.toString());
+                    try {
+                        JSONArray jArray = jsonObject.getJSONArray("geonames");
+                        for (int i=0;i<jArray.length();i++){
+                            JSONObject jObject = jArray.getJSONObject(i);
+                            String cName = jObject.getString("name");
+                            Log.d("<-->CityName<-->",cName);
+                        }
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+
                     hideProgressDialog();
                 }
             }, new Response.ErrorListener() {
@@ -63,17 +82,18 @@ public class MainActivity extends Activity implements View.OnClickListener {
                     Log.d(TAG + "Error", volleyError.toString());
                     hideProgressDialog();
                 }
-            }){
+            });
+            /*{
 
                 @Override
                 public Map<String, String> getHeaders() throws AuthFailureError {
-                    HashMap<String, String> headers = new HashMap<String, String>();
                     headers.put("Content-Type", "application/json");
-                    return headers;
+                    HashMap<String, String> headers = new HashMap<String, String>();
+                   return headers;
                 }
 
                 @Override
-                public Map<String, String> getParams(){
+                public Map<String, String> getParams() throws AuthFailureError {
                     Map<String, String> params = new HashMap<String,String>();
                     params.put("username", strUsername);
                     params.put("password", strPassword);
@@ -81,19 +101,18 @@ public class MainActivity extends Activity implements View.OnClickListener {
 
                     return params;
                 }
-            };
+            };*/
 
-            AppController.getInstance().addToRequestQueue(jObjReq,"json_object_request");
+//            AppController.getInstance().addToRequestQueue(jObjReq,"json_object_request");
 
+            RequestQueue requestQueue = Volley.newRequestQueue(this);
+            requestQueue.add(jObjReq);
         }
 
     @Override
     public void onClick(View v) {
         if(v == register){
-            strUsername = uName.getText().toString().trim();
-            strPassword = pWord.getText().toString().trim();
-            strGroupname = gpName.getText().toString().trim();
-            getJsonObjectReq(strUsername, strPassword, strGroupname);
+            getJsonObjectReq();
         }
     }
 
